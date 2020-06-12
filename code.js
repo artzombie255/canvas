78838189c2cdcec25d173318911e80f93a086ae4
artzombie <artzombie255@gmail.com>
2020-06-12 13:53:09 -0500

    cool

--- code.js
+++ code.js
@@ -1,14 +1,14 @@
+const width = 300;
+const height = 300;
+
 const canvas = document.getElementById("sandbox");
-canvas.style.width = "600px";
-canvas.style.height = "300px";
+canvas.style.width = width + "px";
+canvas.style.height = height + "px";
 
 function draw() {
   const context = canvas.getContext("2d");
-  context.fillStyle = "#" + Math.floor(Math.random() * 16777215).toString(16);
-  context.fillRect(
-    Math.random() * 300,
-    Math.random() * 300,
-    Math.random() * 300,
-    Math.random() * 300
-  );
+  for (var i = 0; i < 150; i++) {
+    context.fillStyle = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    context.fillRect(i, i, i, i);
+  }
 }
