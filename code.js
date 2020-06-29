7d992c4011d9ea7fa4995df3e06110753c6d8032
PhoenixR <artzombie255@gmail.com>
2020-06-29 16:05:54 -0500

    rolls its eyes!

--- code.js
+++ code.js
@@ -19,24 +19,25 @@ const width = 300;
 const height = 300;
 
 var count = 0;
+var elapsedQuarterSeconds = 0;
 const canvas = document.getElementById("sandbox");
+const context = canvas.getContext("2d");
 canvas.style.width = width + "px";
 canvas.style.height = height + "px";
 
 function startDrawing() {
-  window.requestAnimationFrame(draw);
+  window.requestAnimationFrame(drawAnimation);
 }
 
 function draw() {
-  const context = canvas.getContext("2d");
   context.fillStyle = "white";
   context.fillRect(0, 0, 300, 300);
 
   context.lineWidth = 5;
 
-  var offset = count * 0.1;
+  // var offset = count * 0.1;
   context.beginPath();
-  context.arc(125 + offset, 125, 100, 0, 2 * Math.PI);
+  context.arc(125, 125, 100, 0, 2 * Math.PI);
   // context.strokeStyle = "red";
   context.stroke();
   context.fillStyle = "tan";
@@ -80,7 +81,193 @@ function draw() {
   context.stroke();
   context.fill();
 
-  console.log("finished rendering frame ", count);
+  // console.log("finished rendering frame ", count);
+  // count++;
+  // window.requestAnimationFrame(draw);
+}
+
+function drawAnimation() {
+  context.fillStyle = "white";
+  context.fillRect(0, 0, 300, 300);
+
+  context.lineWidth = 5;
+
+  // var offset = count * 0.1;
+  context.beginPath();
+  context.arc(125, 125, 100, 0, 2 * Math.PI);
+  // context.strokeStyle = "red";
+  context.stroke();
+  context.fillStyle = "tan";
+  context.fill();
+
+  context.beginPath();
+  context.arc(125, 150, 50, 0, Math.PI);
+  // context.strokeStyle = "blue";
+  context.stroke();
+
+  context.beginPath();
+  context.arc(75, 100, 20, 0, 2 * Math.PI);
+  context.stroke();
+  context.fillStyle = "white";
+  context.fill();
+
+  context.beginPath();
+  context.arc(175, 100, 20, 0, 2 * Math.PI);
+  context.stroke();
+  context.fill();
+  if (elapsedQuarterSeconds < 1) {
+    context.beginPath();
+    context.arc(70, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(170, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(70, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(170, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+
+  if (elapsedQuarterSeconds === 1) {
+    context.beginPath();
+    context.arc(70, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(170, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(70, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(170, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+
+  if (elapsedQuarterSeconds === 2) {
+    context.beginPath();
+    context.arc(75, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(175, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(75, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(175, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+
+  if (elapsedQuarterSeconds === 3) {
+    context.beginPath();
+    context.arc(80, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 95, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(80, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 95, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+  if (elapsedQuarterSeconds === 4) {
+    context.beginPath();
+    context.arc(80, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(80, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+  if (elapsedQuarterSeconds === 5) {
+    context.beginPath();
+    context.arc(80, 105, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "blue";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 105, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+
+    context.beginPath();
+    context.arc(80, 105, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "black";
+    context.fill();
+
+    context.beginPath();
+    context.arc(180, 105, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  }
+
+  if (elapsedQuarterSeconds > 5) {
+    draw();
+    elapsedQuarterSeconds = 0;
+    window.requestAnimationFrame();
+    return;
+  }
+  // console.log("finished rendering frame ", count);
   count++;
-  window.requestAnimationFrame(draw);
+  if (count % 2.5 === 0) {
+    elapsedQuarterSeconds++;
+    console.log("elapsed quarter seconds ", elapsedQuarterSeconds);
+  }
+  window.requestAnimationFrame(drawAnimation);
 }
