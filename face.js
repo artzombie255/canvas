e9c7d5ae1f16f4ac398e9f6c4f874cb96ecccb52
Your Name <you@example.com>
2021-10-10 07:59:16 -0500

    2nd face

--- face.js
+++ face.js
@@ -492,8 +492,66 @@ function drawAnimation() {
 //   }
 // }
 function SSB() {
-  context.fillStyle = "black";
-  context.fillRect(0, 0, 300, 300);
+  function startDrawing() {
+    window.requestAnimationFrame(drawAnimation);
+  }
+  
+  function draw() {
+    context.fillStyle = "white";
+    context.fillRect(0, 0, 300, 300);
+  
+    context.lineWidth = 5;
+  
+    // var offset = count * 0.1;
+    context.beginPath();
+    context.arc(125, 125, 100, 0, 2 * Math.PI);
+    // context.strokeStyle = "red";
+    context.stroke();
+    context.fillStyle = "tan";
+    context.fill();
+  
+    context.beginPath();
+    context.arc(125, 150, 50, 0, Math.PI);
+    // context.strokeStyle = "blue";
+    context.stroke();
+  
+    context.beginPath();
+    context.arc(75, 100, 20, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "white";
+    context.fill();
+  
+    context.beginPath();
+    context.arc(175, 100, 20, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  
+    context.beginPath();
+    context.arc(70, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "red";
+    context.fill();
+  
+    context.beginPath();
+    context.arc(170, 100, 12.5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  
+    context.beginPath();
+    context.arc(70, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fillStyle = "red";
+    context.fill();
+  
+    context.beginPath();
+    context.arc(170, 100, 5, 0, 2 * Math.PI);
+    context.stroke();
+    context.fill();
+  
+    // console.log("finished rendering frame ", count);
+    // count++;
+    // window.requestAnimationFrame(draw);
+  }
 }
 function rollADie() {
   context.fillStyle = "white";
